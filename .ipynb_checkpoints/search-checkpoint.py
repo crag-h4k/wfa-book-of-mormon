033ef919f190c4c09343f59120d91088c964fdb1
l0k1 <l0k1@l0k1>
2019-01-11 11:45:34 -0700

    initial work started

--- .ipynb_checkpoints/search-checkpoint.py
+++ .ipynb_checkpoints/search-checkpoint.py
@@ -5,10 +5,10 @@ def find_word():
     return
 def get_data(fname):
     with open(fname, 'r') as f:
-        raw = f.read()
-    return raw
-    #return loads(raw)
+        _data = f.read()
+
+    return loads(_data)
 
 #if __name__ == '__main__':
-#raw = get_data('book-of-mormon.json')
-#print(raw[:5])
+raw = get_data('book-of-mormon.json')
+print(raw[:5])
---
+++ Verse.py
@@ -0,0 +1,7 @@
+class Verse:
+    def __init__(self, word, reference, count):
+        self.word = word
+        self.reference = reference
+        self.count = count
+
+
