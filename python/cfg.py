d9157f43dd33293eab819e148763ca11d7a621c2
f3nr1r <f3nr1r@f3nr1r.frig_off.net>
2019-04-09 08:13:35 -0600

    books won't import from cfg

--- python/cfg.py
+++ python/cfg.py
@@ -9,16 +9,24 @@ common = [
     '', '', '',
     '', '', '',
     '', '', '',]
+books = ['1 Nephi', '2 Nephi', 'Jacob', 'Enos', 
+        'Jarom', 'Omni', 'Words of Mormon', 'Mosiah', 
+        'Alma', 'Helaman', '3 Nephi', '4 Nephi', 
+        'Mormon', 'Ether', 'Moroni',]
+gospel_steps = ['faith', 'repentance', 'repent', 'baptism',
+               'baptize', 'ghost', 'gift','endure']
+
 jesus_aliases = [
     'advocate', 'alpha', 'omega', 'anointed',
     'ascension', 'atone, atonement', 'beginning',
-    'begotten', 'blood', 'bridegroom', 'comforter',
+    'begotten', 'blood', 'bridegroom', 'christ',
+    'comforter',
     'conscience', 'cornerstone', 'creator', 'cross',
     'crucifixion', 'deliverer', 'endless', 'faith',
     'firstborn', 'god', 'godhead', 'shepherd',
     'gospels', 'grace', 'immanuel', 'jehovah',
-    'lamb', 'light', 'lord', 'mediator',
+    'lamb', 'light', 'lord', 'jesus', 'mediator',
     'messiah', 'redeem', 'redeemed', 'redemption',
     'redeemer', 'resurrection', 'rock', 'sacrament',
-    'sacrifice', 'savior', 'way',
+    'sacrifice', 'savior', 'son', 'way',
 ]
